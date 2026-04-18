ce22cb4c3b53eb45c8d2fadf7f6d738211709431
Fati89 <fatimazahraee989@gmail.com>
2026-04-18 21:51:53 +0100

    suppression des produits

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {NgForOf} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-product',
@@ -8,10 +8,21 @@ import {NgForOf} from '@angular/common';
   styleUrl: './product.css',
   standalone: true
 })
-export class Product {
-  products = [
-      {id: 1, name: 'Computer', price: 2300, selected: true},
-      {id: 2, name: 'Printer', price: 1200, selected: false},
-      {id: 3, name: 'Smart Phone', price: 1100, selected: true}
-  ]
+export class Product implements OnInit{
+  products! : Array<any>;
+  Constructor(){}
+  ngOnInit(){
+    this.products = [
+          {id: 1, name: 'Computer', price: 2300, selected: true},
+          {id: 2, name: 'Printer', price: 1200, selected: false},
+          {id: 3, name: 'Smart Phone', price: 1100, selected: true}
+      ]
+  }
+  handelDelete(product: any){
+    let v = confirm('Etes vous sure de vouloir supprimer ce produit ?');
+    if (v){
+      this.products = this.products.filter((p:any)=>p.id != product.id);
+    }
+
+  }
 }
